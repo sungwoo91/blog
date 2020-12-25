2f4517180e5a5d5c738ee8d9c87329a4ddad989c
leesungwoo <tols3319@gmail.com>
2020-12-25 23:11:26 +0900

    icon변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         background_color: fullConfig.theme.colors.white,
         theme_color: fullConfig.theme.colors.green["500"],
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
+        icon: `src/images/toto.jpeg`,
       },
     },
     {
